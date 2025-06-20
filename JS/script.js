f974a262e9a23bdde0b93c86e8b56126358c57a2
Leo-bL <hamode.alblati@gmail.com>
2025-06-20 14:32:41 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68553fc7107032190c97c35d/1iu6g2908';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
